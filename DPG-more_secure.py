986fb9e42335b2010b2876efd02f0f994f7031af
rishabh jain <66913499+rishabhjainfinal@users.noreply.github.com>
2020-09-05 17:46:36 +0530

    Add files via upload

---
+++ DPG-more_secure.py
@@ -0,0 +1,15 @@
+from DicewarePasswordGenerator import main as password 
+
+
+def main():
+	paslist=password().split(' ')
+	lastword = []
+	for i in paslist:
+		lastword.append(i[0])
+
+	paslist.append(''.join(lastword))
+	return " ".join(paslist)
+
+
+if __name__ == '__main__':
+	print(main())
